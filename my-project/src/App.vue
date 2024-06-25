6a36bae123263a91d9f386734fa3ff25bfaa0ecc
Eduardo <ecuadra.git@gmail.com>
2024-06-24 23:03:14 -0500

    first commit

---
+++ my-project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="flex flex-col h-screen ">
+
+    
+    <Aside></Aside>  
+
+    <Header></Header>  
+
+</div>
+</template>
+
+<script setup>
+
+import Aside from './components/Aside.vue';
+    import  Header  from "./components/Header.vue";
+
+</script>
